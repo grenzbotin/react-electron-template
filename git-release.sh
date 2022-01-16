e55d915dc7fc7eafb623bea64d73fa9b0bc641e0
grenzbotin <grenzbotin@googlemail.com>
2022-01-16 14:05:13 +0100

    release version '0.0.5'

---
+++ git-release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+gitRelease() {
+    echo '📝 Specify version'
+    yarn version
+
+    REACT_APP_VERSION="$(node -p -e "require('./package.json').version")"
+
+    git add .
+    git commit -m "release version '$REACT_APP_VERSION'"
+
+    git push origin --tags
+
+    echo '🚀 Pushed new version: '$REACT_APP_VERSION''
+}
+
+gitRelease
